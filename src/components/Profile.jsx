defd3a83986b89363fdcd77faa252134e418a637
Andy <alipinski@mail.com>
2022-10-13 17:01:28 +0200

    more styling, icons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,27 @@
+import { BsTwitter } from 'react-icons/bs';
+import { SiFacebook } from 'react-icons/si';
+import { AiOutlineGooglePlus } from 'react-icons/ai';
+import { IconContext } from 'react-icons';
+import Card from './Card';
+
 const Profile = ({ name, job, img }) => {
   return (
-    <div className='Profile'>
-      <img src={img} alt='person' />
-      <p>Name: {name}</p>
-      <p> Job: {job}</p>
-    </div>
+    <Card>
+      <div className='profile'>
+        <img src={img} alt='person' />
+        <div className='profile-content'>
+          <div className='text'>
+            <p> {name}</p>
+            <p> {job}</p>
+          </div>
+          <IconContext.Provider value={{ color: '#666', size: '20px' }}>
+            <BsTwitter />
+            <SiFacebook />
+            <AiOutlineGooglePlus />
+          </IconContext.Provider>
+        </div>
+      </div>
+    </Card>
   );
 };
 
--- src/components/ProfileList.jsx
+++ src/components/ProfileList.jsx
@@ -3,16 +3,19 @@ import { profileData } from '../profileData';
 
 const ProfileList = () => {
   return (
-    <div>
-      {profileData.map((profile) => (
-        <Profile
-          key={profile.id}
-          name={profile.name}
-          job={profile.job}
-          img={profile.img}
-        />
-      ))}
-    </div>
+    <>
+      <h1>Team members</h1>
+      <div className='profile-container'>
+        {profileData.map((profile) => (
+          <Profile
+            key={profile.id}
+            name={profile.name}
+            job={profile.job}
+            img={profile.img}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
